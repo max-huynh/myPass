da9308cf722ca91174f58a696128ce9d0850a4e4
root <root@Ryzen-Taipei.localdomain>
2020-05-03 23:55:02 +1000

    Initial commit, app skeleton

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    now = datetime.datetime.now()
+    timeString = now.strftime("%Y-%m-%d %H:%M")
+    templateData = {
+            'title' : 'Hello!',
+            'time' : timeString
+            }
+    return render_template('main.html', **templateData)
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
